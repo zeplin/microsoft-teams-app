405bf00561b3b94adf88b34774e1f7cc6f7510b3
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2020-07-13 10:28:47 +0300

    Add eslint setup

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -1,5 +1,6 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from "next";
+import { OK } from "http-status-codes";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).json({ status: "pass" });
-}
+export default (req: NextApiRequest, res: NextApiResponse): void => {
+    res.status(OK).json({ status: "pass" });
+};
