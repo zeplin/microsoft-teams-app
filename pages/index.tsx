0ba03999135f3ece9bd548d6e13ba11a6bcbff4e
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2020-07-13 10:28:47 +0300

    Add eslint setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
-import React from 'react';
+import React, { FunctionComponent } from "react";
 
-export default () => <div>Home</div>;
+const HomePage: FunctionComponent = () => <div>Home</div>;
+
+export default HomePage;
