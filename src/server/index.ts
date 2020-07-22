ef661e134bfc13afa21fbe2faf63dee645860fc2
ergunsh <erdogmusergun@gmail.com>
2020-07-22 14:29:58 +0300

    Use express in the server side

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,22 +1,20 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import next from 'next'
+import { app } from "./app";
 
-const port = parseInt(process.env.PORT || '3000', 10)
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+// eslint-disable-next-line no-process-env
+const ENVIRONMENT = process.env.NODE_ENV;
+// eslint-disable-next-line no-process-env
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
+async function drive(): Promise<void> {
+    const dev = ENVIRONMENT !== "production";
+    // Initialize environment variables and the app
+    await app.init({ dev });
 
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
-})
+    // Listen for the requests
+    await app.listen(port);
+
+    // eslint-disable-next-line no-console
+    console.log(`> Server listening at http://localhost:${port} as ${dev ? "development" : ENVIRONMENT}`);
+}
+
+drive();
