f94ad28b9e496ee028facd93318b2f2f32750b21
ergunsh <erdogmusergun@gmail.com>
2020-07-27 13:38:28 +0300

    Add unit test for `messageJobRepo`

--- src/server/adapters/redis.ts
+++ src/server/adapters/redis.ts
@@ -4,7 +4,6 @@ import { promisify } from "util";
 class Redis {
     private redis!: RedisClient;
     close!: () => Promise<"OK">;
-    get!: (key: string) => Promise<string>;
     set!: (key: string, val: string) => Promise<"OK">;
     flush!: () => Promise<string>;
     lpush!: (key: string, ...args: string[]) => Promise<number>;
@@ -12,10 +11,22 @@ class Redis {
     init(url: string): void {
         this.redis = createClient(url);
         this.close = promisify(this.redis.quit).bind(this.redis);
-        this.get = promisify(this.redis.get).bind(this.redis);
         this.set = promisify(this.redis.set).bind(this.redis);
         this.flush = promisify<string>(this.redis.flushall).bind(this.redis);
         this.lpush = promisify(this.redis.lpush).bind(this.redis);
+    }
+
+    get(key: string): Promise<string|null> {
+        return new Promise((resolve, reject) => {
+            this.redis.get(key, (err, result) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                resolve(result);
+            });
+        });
     }
 
     /**
