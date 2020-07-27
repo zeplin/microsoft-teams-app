26115b29a742ddc69418055db769732f434d55f2
ergunsh <erdogmusergun@gmail.com>
2020-07-27 16:08:45 +0300

    Add coverage collection related config to jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    moduleDirectories: ["node_modules", "src"]
+    moduleDirectories: ["node_modules", "src"],
+    coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
+    modulePathIgnorePatterns: ["<rootDir>/dist/"],
+    collectCoverageFrom: [
+        "<rootDir>/src/**/*.ts",
+        "<rootDir>/src/**/*.tsx",
+        "!<rootDir>/src/server/test/**/*.ts"
+    ]
 };
