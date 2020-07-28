7f75a55cf1c410617b39a785ee0d6e52d6126bd0
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2020-07-28 10:35:58 +0300

    Refactor routing (#16)

--- src/server/messaging/index.ts
+++ src/server/messaging/index.ts
@@ -1,2 +1,1 @@
-export * from "./messageController";
 export * from "./messaging";
--- src/server/messaging/messaging.ts
+++ src/server/messaging/messaging.ts
@@ -1,11 +1,14 @@
 import { messageQueue } from "./messageQueue";
 import { messageFacade } from "./messageFacade";
+import { Router } from "express";
+import { messageRouter } from "./messageRouter";
 
 type MessagingConfig = {
     REDIS_URL: string;
 }
 
-export function initMessaging(config: MessagingConfig): void {
+export function initMessaging(router: Router, config: MessagingConfig): void {
     messageQueue.init({ REDIS_URL: config.REDIS_URL });
     messageQueue.process(job => messageFacade.processJob(job.data));
+    router.use("/webhook", messageRouter);
 }
