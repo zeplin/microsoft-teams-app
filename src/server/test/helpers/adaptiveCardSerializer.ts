fe0f30b58956b734e802fde3b59a719c6ae1b69a
ergunsh <erdogmusergun@gmail.com>
2020-08-07 16:12:01 +0300

    Remove unnecessary indent

--- src/server/test/helpers/adaptiveCardSerializer.ts
+++ src/server/test/helpers/adaptiveCardSerializer.ts
@@ -1,10 +1,10 @@
-const PRETTY_INDENT = 4;
+const PRETTY_INDENT = 2;
 const adaptiveCardSerializer: jest.SnapshotSerializerPlugin = {
     test(value): boolean {
         return typeof value === "object" && value.type === "AdaptiveCard";
     },
-    print(value, _, indent): string {
-        return indent(JSON.stringify(value, null, PRETTY_INDENT));
+    print(value): string {
+        return JSON.stringify(value, null, PRETTY_INDENT);
     }
 };
 
