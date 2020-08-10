e956b367b336bd99729a661cf9715045e7dc59ed
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2020-08-10 16:54:35 +0300

    Use children instead of content prop

--- src/client/containers/Home/components/Login.tsx
+++ src/client/containers/Home/components/Login.tsx
@@ -11,21 +11,19 @@ export const Login: FunctionComponent<LoginProps> = ({ onTokenReceive }) => (
     <Flex fill column gap="gap.small" styles={{ padding: "60px" }}>
         <Text size="large" content="One more step before finishing connection" weight="semibold" />
         <Flex fill column gap="gap.medium">
-            <Text
-                content={
-                    "Once proceeding, we will ask you to authorize Microsoft Teams access through a separate " +
-                "window. Once it’s authorized, you can do more advanced settings on the Teams channel linked " +
-                "with Zeplin."
-                }
-            />
+            <Text>
+                Once proceeding, we will ask you to authorize Microsoft Teams access through a separate window. Once
+                it’s authorized, you can do more advanced settings on the Teams channel linked with Zeplin.
+            </Text>
             <Button
-                content="Log in Zeplin"
                 onClick={(): void => {
                     microsoftTeams.authentication.authenticate({
                         successCallback: onTokenReceive,
                         url: `${BASE_URL}/zeplin/auth/start`
                     });
-                }} />
+                }}>
+                Log in Zeplin
+            </Button>
         </Flex>
     </Flex>
 );
