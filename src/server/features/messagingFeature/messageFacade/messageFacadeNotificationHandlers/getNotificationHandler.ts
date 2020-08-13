ec03804f35b68bf46ce0c4a36c7cc7ab331168eb
ergunsh <erdogmusergun@gmail.com>
2020-08-13 14:13:51 +0300

    Add new handlers to `notificationMap` object

--- src/server/features/messagingFeature/messageFacade/messageFacadeNotificationHandlers/getNotificationHandler.ts
+++ src/server/features/messagingFeature/messageFacade/messageFacadeNotificationHandlers/getNotificationHandler.ts
@@ -2,10 +2,14 @@ import { projectColorNotificationHandler } from "./projectColorNotificationHandl
 import { NotificationHandler } from "./NotificationHandler";
 import { EventType } from "../../messagingTypes";
 import { styleguideColorNotificationHandler } from "./styleguideColorNotificationHandler";
+import { projectNoteNotificationHandler } from "./projectNoteNotificationHandler";
+import { projectNoteCommentNotificationHandler } from "./projectNoteReplyNotificationHandler";
 
 const notificationMap: Record<EventType, NotificationHandler> = {
     [EventType.PROJECT_COLOR]: projectColorNotificationHandler,
-    [EventType.STYLEGUIDE_COLOR]: styleguideColorNotificationHandler
+    [EventType.STYLEGUIDE_COLOR]: styleguideColorNotificationHandler,
+    [EventType.PROJECT_NOTE]: projectNoteNotificationHandler,
+    [EventType.PROJECT_NOTE_COMMENT]: projectNoteCommentNotificationHandler
 } as const;
 
 export function getNotificationHandler(eventType: EventType): NotificationHandler {
