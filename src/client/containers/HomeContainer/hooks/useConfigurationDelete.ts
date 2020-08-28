535abe02fea76325889e193cce70f027f9d92ffe
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2020-08-28 12:27:22 +0300

    Revert latest fix since it produce error silently

--- src/client/containers/HomeContainer/hooks/useConfigurationDelete.ts
+++ src/client/containers/HomeContainer/hooks/useConfigurationDelete.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useMutation } from "react-query";
 import * as microsoftTeams from "@microsoft/teams-js";
 
@@ -6,27 +6,36 @@ import { fetchConfigurationDelete } from "../../../requester";
 import { State } from "./useHomeReducer";
 
 export const useConfigurationDelete = (state: State): void => {
+    /**
+     * Workaround: microsoftTeams.settings.registerOnRemoveHandler produce error when it's called multiple times.
+     * As workaround microsoftTeams.settings.registerOnRemoveHandler called once and get the value of accessToken via useRef
+     * For more info: https://reactjs.org/docs/hooks-faq.html#how-to-read-an-often-changing-value-from-usecallback
+     */
+    const accessTokenRef = useRef(state.accessToken);
+
+    useEffect(() => {
+        accessTokenRef.current = state.accessToken;
+    }, [state.accessToken]);
+
     const [deleteConfiguration] = useMutation(fetchConfigurationDelete, { throwOnError: true });
 
     useEffect(() => {
         if (state.configurationId) {
-            microsoftTeams.getContext(() => {
-                microsoftTeams.settings.registerOnRemoveHandler(async removeEvent => {
-                    if (state.accessToken) {
-                        try {
-                            await deleteConfiguration({
-                                accessToken: state.accessToken,
-                                configurationId: state.configurationId
-                            });
-                            removeEvent.notifySuccess();
-                        } catch (error) {
-                            removeEvent.notifyFailure(error?.message ?? error);
-                        }
-                    } else {
-                        removeEvent.notifyFailure("access token not found");
+            microsoftTeams.settings.registerOnRemoveHandler(async removeEvent => {
+                if (accessTokenRef.current) {
+                    try {
+                        await deleteConfiguration({
+                            accessToken: accessTokenRef.current,
+                            configurationId: state.configurationId
+                        });
+                        removeEvent.notifySuccess();
+                    } catch (error) {
+                        removeEvent.notifyFailure(error?.message ?? error);
                     }
-                });
+                } else {
+                    removeEvent.notifyFailure("access token required");
+                }
             });
         }
-    }, [state.configurationId, state.accessToken]);
+    }, []);
 };
