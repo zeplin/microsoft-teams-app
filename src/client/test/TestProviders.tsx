a0e841677a591d3d05200899e7c9328497069ca4
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2020-09-03 15:39:50 +0300

    Fix merge conflict

--- src/client/test/TestProviders.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FunctionComponent } from "react";
-import { Provider } from "@fluentui/react-northstar";
-
-export const TestProviders: FunctionComponent = ({ children }) => (
-    <Provider>
-        {children}
-    </Provider>
-);
