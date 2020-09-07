2d2984724f56fed05c450d3c1221122f0fde50c9
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2020-09-07 14:29:34 +0300

    Fix config repo constructor

--- src/server/repos/configurationRepo/configurationRepo.ts
+++ src/server/repos/configurationRepo/configurationRepo.ts
@@ -25,13 +25,16 @@ interface ConfigurationCreateParameters {
 type Configuration = Pick<ConfigurationDocument, "_id" | "zeplin" | "microsoftTeams">;
 
 class ConfigurationRepo {
-    model: Model<ConfigurationDocument>;
+    model?: Model<ConfigurationDocument>;
 
-    constructor() {
-        this.model = mongo.createModel("configuration", configurationSchema);
+    private getModel(): Model<ConfigurationDocument> {
+        return mongo.createModel("configuration", configurationSchema);
     }
 
     async getByWebhookId(webhookId: string): Promise<Configuration | null> {
+        if (!this.model) {
+            this.model = this.getModel();
+        }
         const result = await this.model.findOne({
             "zeplin.webhookId": webhookId
         }).lean().exec();
@@ -39,15 +42,24 @@ class ConfigurationRepo {
     }
 
     async get(id: string): Promise<Configuration | null> {
+        if (!this.model) {
+            this.model = this.getModel();
+        }
         const result = await this.model.findById(id).lean().exec();
         return result ?? null;
     }
 
     async delete(id: string): Promise<void> {
+        if (!this.model) {
+            this.model = this.getModel();
+        }
         await this.model.deleteOne({ _id: id }).lean().exec();
     }
 
     async create(parameters: ConfigurationCreateParameters): Promise<Configuration> {
+        if (!this.model) {
+            this.model = this.getModel();
+        }
         const { _id, zeplin, microsoftTeams } = await this.model.create(parameters);
         return { _id, zeplin, microsoftTeams };
     }
